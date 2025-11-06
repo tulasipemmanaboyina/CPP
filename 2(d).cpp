97c1c118fd7ec0abc3e5bb26674aa92cb9c345ff
PEMMANABOYINA TULASI <tulasipemmanaboina@gmail.com>
2025-11-06 20:11:51 +0530

    Create 2(d).cpp

---
+++ 2(d).cpp
@@ -0,0 +1,29 @@
+// Constructor overloading
+#include<iostream>
+using namespace std;
+class Demo
+{
+	public:
+			Demo()
+			{
+				cout<<"I am in Default Constructor";
+			}
+			Demo(int x)
+			{
+				cout<<"\nI am in One arg constructor"<<x;
+			}
+			Demo(char str[12])
+			{
+				cout<<"\nI am in one arg string constructor"<<str;
+			}
+			~Demo()
+			{
+				cout<<"\nI am in DESTRUCTOR";
+			}
+};
+int main()
+{
+	Demo ob;
+	Demo ob1(10);
+	Demo ob2("CPP Demo Code");
+}
