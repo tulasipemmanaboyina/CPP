e2f544e18682509aa48d3587834e9e9a5ea65fd0
PEMMANABOYINA TULASI <tulasipemmanaboina@gmail.com>
2025-11-06 20:20:29 +0530

    Update 3e.cpp

--- 3e.cpp
+++ 3e.cpp
@@ -22,8 +22,7 @@ int main() {
     s.display();
     return 0;
 }
-OUTPUT:
- 
+
 2) // Demonstration :: Pointer to a Class
 #include <iostream>
 using namespace std;
@@ -41,3 +40,52 @@ int main() {
     ptr->showLength();
     return 0;
 }
+3) // Demonstration of "this" Pointer
+#include <iostream>
+using namespace std;
+class Test {
+    int x;
+public:
+    Test(int x) //  local varible is X , argument also X so, this pointer will be used
+	{
+        this->x = x; // Resolving ambiguity
+    }
+    void show() { cout << "Value of x: " << this->x << endl; }
+};
+int main() {
+    Test t(50);
+    t.show();
+    return 0;
+}
+4) // Demonstrtion of Virtual Base Class
+#include <iostream>
+using namespace std;
+class Person 
+{	public:
+    void show() 
+	{ cout << "I am a Person" << endl; }
+};
+class Student : virtual public Person 
+{	public:
+    void studentInfo() { cout << "I am a Student" << endl; }
+};
+class Teacher : virtual public Person 
+{	public:
+    void teacherInfo() { cout << "I am a Teacher" << endl; }
+};
+class TA : public Student, public Teacher 
+{	public:
+    void display() 
+	{	show(); // Only one copy of Person is inherited
+    }
+};
+int main() {
+    TA obj;
+    obj.display();
+    obj.studentInfo();
+    obj.teacherInfo();
+    return 0;
+}
+
+
+
